1ef8aad5aa300f32a6f53e988c08cf2537291803
Paweł <pawel_gnat@o2.pl>
2023-10-11 20:36:28 +0200

    feat: protected home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,28 @@
+import { NextPageContext } from 'next'
+import { getSession, signOut } from 'next-auth/react'
+
+export async function getServerSideProps(context: NextPageContext) {
+	const session = await getSession(context)
+
+	if (!session) {
+		return {
+			redirect: {
+				destination: '/auth',
+				permanent: false
+			}
+		}
+	}
+
+	return {
+		props: {}
+	}
+}
+
 export default function Home() {
 	return (
 		<>
 			<h1>Netflix Clone</h1>
+			<button onClick={() => signOut()}>Logout</button>
 		</>
 	)
 }
